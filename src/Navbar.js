2d42f944d9526ff66682d70c5d248e1ed32eceac
Zagarsix <claudk1006@gmail.com>
2024-01-17 00:54:53 -0300

    modifying navbar and playing with margins in index.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,26 +3,36 @@ import logoCC from "./media/logoDark.png"
 
 function Navbar() {
     return (
-        <nav class="navbar nav-underline fixed-top" style={{background:"black"}}>
-            <div class="container-fluid justify-content-betweeen" style={{background:"black"}}>
-                <a class="navbar-brand" href="#">
-                    <img src={logoCC} alt="Bootstrap" width="270" height="100"></img>
+        <nav class="navbar fixed-top" style={{ background: "black" }}>
+            <div class="container-fluid justify-content-between">
+                <a class="navbar-brand" href="/">
+                    <img src={logoCC} alt="Zagarsix" width="270" height="100"></img>
                 </a>
-                <ul class="nav nav-underline justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Introduction">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Disabled</a>
-                    </li>
-                </ul>
+                <div class="section" style={{background:"green"}}>
+                <nav class="navbar px-5 mx-5" style={{background:"pink"}}>
+                    <ul class="nav nav-underline">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/Introduction">Introducción</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Habilidades</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Lenguajes y herramientas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contáctame</a>
+                        </li>
+                    </ul>
+                </nav>
+                </div>
+                <div style={{background:"green"}}>
+                    <p>Hola Mundo</p>
 
+                </div>
             </div>
         </nav>
     )
